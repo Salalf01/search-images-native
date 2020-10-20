8d3d03fcfa00b072c8a82dc0225284345e6486f5
Salomon Martinez <salomom.lugo@gmail.com>
2020-10-20 19:31:09 -0400

    api key

--- modules/home/actions.js
+++ modules/home/actions.js
@@ -1,15 +1,14 @@
+require('dotenv').config()
 
 export const fetchImages = () => {
-    const key = "f7d226922ac35920187c75066c2e58d1a954bf720873c494bb38ea673990532e";
-   return fetch(`https://api.unsplash.com/photos/?client_id=${key}`, {
+   return fetch(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_API_KEY}`, {
        method: 'GET',
     }).then((response) => response.json()).then((data) => data);
 }
 
 
 export const searchImages = (search) => {
-    const key = "f7d226922ac35920187c75066c2e58d1a954bf720873c494bb38ea673990532e";
-   return fetch(`https://api.unsplash.com/search/photos/?query=${search}&client_id=${key}`, {
+   return fetch(`https://api.unsplash.com/search/photos/?query=${search}&client_id=${process.env.REACT_APP_API_KEY}`, {
        method: 'GET',
     }).then((response) => response.json()).then((data) => data);
 }
