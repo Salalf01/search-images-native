1d9600e7828f5d1e5f1088569c6f121d9368c623
Salomon Martinez <salomom.lugo@gmail.com>
2020-10-20 19:45:45 -0400

    add dot env

--- modules/home/actions.js
+++ modules/home/actions.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
+import {API_KEY } from '@env';
 
 export const fetchImages = () => {
-   return fetch(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_API_KEY}`, {
+   return fetch(`https://api.unsplash.com/photos/?client_id=${API_KEY}`, {
        method: 'GET',
     }).then((response) => response.json()).then((data) => data);
 }
 
 
 export const searchImages = (search) => {
-   return fetch(`https://api.unsplash.com/search/photos/?query=${search}&client_id=${process.env.REACT_APP_API_KEY}`, {
+   return fetch(`https://api.unsplash.com/search/photos/?query=${search}&client_id=${API_KEY}`, {
        method: 'GET',
     }).then((response) => response.json()).then((data) => data);
 }
